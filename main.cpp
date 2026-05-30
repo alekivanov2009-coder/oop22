ac7207cd97d328c11d42ae5e02aa76538f95eea5
alekivanov2009-coder <alekivanov2009@gmail.com>
2026-05-30 17:49:41 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,32 +1,62 @@
 #include <iostream>
 #include "Manager.h"
+#include "User.h"
 
 using namespace std;
 
 int main() {
 
     Manager manager(1, "Ivan");
+    User user1(2, "Maria", "maria@email.com");
 
-    Project project = manager.createProject(1, "Task System", "C++ OOP Project");
+    Project project =
+        manager.createProject(
+            1,
+            "Task Manager",
+            "OOP Project"
+        );
 
-    Task t1(1, "Design", "Design classes", 1);
-    Task t2(2, "Implement", "Write code", 2);
+    Task t1(1, "Design", "Create diagram", 1);
+    Task t2(2, "Implementation", "Write code", 2);
+    Task t3(3, "Testing", "Test system", 3);
+
+    t1.assignTo(user1.getName());
+    t1.changeStatus("DONE");
+    t1.addComment(Comment(1, "Finished successfully"));
+
+    t2.changeStatus("IN_PROGRESS");
+
+    t3.changeStatus("NEW");
 
     project.addTask(t1);
     project.addTask(t2);
-
-    project.getTasks()[0].changeStatus("IN_PROGRESS");
-    project.getTasks()[1].changeStatus("DONE");
-
-    project.getTasks()[0].addComment(Comment(1, "Started"));
-    project.getTasks()[1].addComment(Comment(2, "Finished"));
+    project.addTask(t3);
 
     cout << "Project: " << project.getName() << endl;
+    cout << endl;
 
-    for (auto &t : project.getTasks()) {
-        cout << "- Task: " << t.getTitle()
-             << " | Status: " << t.getStatus() << endl;
+    cout << "All tasks:" << endl;
+
+    for (Task &task : project.getTasks()) {
+        cout
+            << task.getTitle()
+            << " | Status: " << task.getStatus()
+            << " | Priority: " << task.getPriority()
+            << " | Assigned to: " << task.getAssignedUser()
+            << endl;
     }
+
+    cout << endl;
+    cout << "Done tasks:" << endl;
+    project.filterByStatus("DONE");
+
+    cout << endl;
+    project.sortByPriority();
+
+    cout << "Progress: "
+         << project.getProgress()
+         << "%"
+         << endl;
 
     return 0;
 }
